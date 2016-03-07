00ead826dfacc97522ae418bd141a5fa19e2ef94
CJ Sampson <cjsampson41@gmail.com>
2016-03-07 10:56:12 -0600

    Added a number function for additional randomness, a little css/html

--- public/php/server-name-generator.php
+++ public/php/server-name-generator.php
@@ -6,9 +6,13 @@ function randomAdjective(){
 	return $adj[randomElement($adj)];
 }
 
-function randomWord(){
+function randomNoun(){
 	$nouns = ['Lion', 'Wolf', 'Books', 'Computer', 'Programmer', 'Table', 'Charger', 'Bear', 'Dirk', 'Jeep'];
 	return $nouns[randomElement($nouns)];
+}
+
+function randomNumber(){
+	return mt_rand(1,99);
 }
 
 function randomElement($arr) {
@@ -22,12 +26,27 @@ function randomElement($arr) {
 <head>
 	<meta charset="UTF-8">
 	<title>Random Name Generator</title>
+	<link rel="stylesheet" href="/css/serverNames.css">
 </head>
 <body>
-	<p><span class="adjective">echo</span><span></span></p>
-	<script>
+
+<div class="serverName">
+	<h4>Congratulations on your new server name!</h4>
+
+	<p><span>You're new server name is: </span>
+	<span id="adjective"><?=randomAdjective()?></span><span id="noun"><?=randomNoun()?></span><span id="number"><?=randomNumber()?></span>
+	</p>
+	<ul>
+		<li>On more instruction on how to set up your server name please click the following links: </li>
+		<li><a href="https://godaddy.com">Go Daddy</a></li>
+		<li><a href="https://digitalOcean">Digital Ocean</a></li>
+		<li><a href="https://forge.laravel.com/">Forge</a></li>
+	</ul>
+</div>
+
+<script>
 		
-	</script>
+</script>
 </body>
 </html>
 
