9797c1dbae7441988413718cd5910913424bb0fa
CJ Sampson <cjsampson41@gmail.com>
2016-03-11 11:18:34 -0600

    Refactored using Input and Log class

--- Auth.php
+++ Auth.php
@@ -10,18 +10,21 @@ class Auth {
 	 	$logger = new Log;
 	 	if($username == 'guest' && password_verify($password, self::$password)) {
 	 		$_SESSION['logged_in_user'] = $username;
-	 		$logger->logInfo('Success');
+	 		$logger->info('Success');
 	 		return true;
 	 	}
-	 	$logger->logError('Failure');
+	 	$logger->error('Failure');
 	 	return false;
 	 }
+
 	 public static function check() {
 	 	return isset($_SESSION['logged_in_user']) ? true : false;
 	 }
+
 	 public static function user(){
 	 	return isset($_SESSION['logged_in_user']) ? $_SESSION['logged_in_user'] : null;
 	 }
+
 	 public static function logout() {
 	 	session_start();
 	 	$_SESSION = [];
