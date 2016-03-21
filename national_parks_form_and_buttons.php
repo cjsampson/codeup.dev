9447a252e783297d0dea0707524c5c81bb30aae1
CJ Sampson <cjsampson41@gmail.com>
2016-03-21 16:51:27 -0500

    added CSS for page

--- national_parks_form_and_buttons.php
+++ national_parks_form_and_buttons.php
@@ -1,35 +1,35 @@
-<!-- Forms for Parks -->
-	<div class="parksForm" method="post">
-		<label for="name">Name: </label>
-		<input type="text" name="name" id="name">
 
-		<label for="location" name="locations">Location: </label>
-		<input type="text" name="location">
+	<!-- Forms for Parks -->
+		<div class="parksForm form-control" method="post">
+			<label for="name">Name: </label>
+			<input type="text" name="name" id="name" placeholder="Name of park">
 
-		<label for="date_established" name="date_established">Date Established: </label>
-		<input type="date" id="date_established" class="datepicker" placeholder="YYYY-MM-DD">
+			<label for="location" name="locations">Location: </label>
+			<input type="text" name="location" placeholder="Location">
 
-		<label for="description" name="description">Description: </label>
-		<textarea name="description" id="description" cols="50" rows="50"></textarea>
+			<label for="date_established" name="date_established">Date Established: </label>
+			<input type="date" id="date_established" class="datepicker" placeholder="YYYY-MM-DD">
 
-		<label for="area_in_acres" name="area_in_acres">Area in Acres: </label>
-		<input type="text" name="area_in_acres" id="area_in_acres">
-		
-		<label for="images">Upload Park Image</label>
-		<input type="file" name="images" id="images">		
-	</div> <!-- .parksForm -->
+			<label for="description" name="description">Description: </label>
+			<textarea name="description" id="description" cols="50" rows="50"></textarea>
 
-
+			<label for="area_in_acres" name="area_in_acres">Area in Acres: </label>
+			<input type="text" name="area_in_acres" id="area_in_acres">
+			
+			<input type="file" name="images" id="images" value="imageOfPark">		
+		</div> <!-- .parksForm -->
 
 
-	<div class="navigationButtons">
-		<?php if ($page == 1): ?>
-			<a href="?page=<?= $page + 1; ?>">Next Page</a>
-		<?php elseif ($page == 2): ?>
-			<a href="?page=<?= $page - 1; ?>">Back</a>
-			<a href="?page=<?= $page + 1; ?>">Next Page</a>
-		<?php else: ?>
-			<a href="?page=<?= $page - 1; ?>">Back</a>
-		<?php endif; ?>
+
+
+		<div class="navigationButtons">
+			<?php if ($page == 1): ?>
+				<a role="button" class="btn btn-default" href="?page=<?= $page + 1; ?>">Next Page</a>
+			<?php elseif ($page == 2): ?>
+				<a role="button" class="btn btn-default" href="?page=<?= $page - 1; ?>">Back Page</a>
+				<a role="button" class="btn btn-default" href="?page=<?= $page + 1; ?>">Next Page</a>
+			<?php else: ?>
+				<a role="button" class="btn btn-default" href="?page=<?= $page - 1; ?>">Back</a>
+			<?php endif; ?>
+		</div>
 	</div>
-</div>
--- national_parks_output.php
+++ national_parks_output.php
@@ -1,14 +1,15 @@
 <div class="nationalParks">
 		<?php foreach ($nationalParks as $nationalPark): ?>
-			<div class="nationalPark">
+			<div class="nationalPark col-md-6">
 				<h4><?= Input::escape($nationalPark['name']); ?></h4>
-				<img src="<?= Input::escape($nationalPark['images']);?> " alt="<?= $nationalPark['name'];?>">
-				<ul class="parkUL">
+				<img src="<?= Input::escape($nationalPark['images']);?> " alt="<?= $nationalPark['name'];?>" class="img-responsive col-md-6">
+				<ul class="parkUL col-md-6">
 					<li>Name: <?= Input::escape($nationalPark['name']); ?></li>
 					<li>Location: <?= Input::escape($nationalPark['location']); ?></li>
 					<li>Date-Established: <?= Input::escape($nationalPark['date_established']); ?></li>
-					<li>Description: <?= Input::escape($nationalPark['description']); ?></li>
 					<li>Area in Acres: <?= Input::escape($nationalPark['area_in_acres']); ?></li>
-				</ul>			
+				</ul>
+				<p class="description col-md-12">Description: <?= Input::escape($nationalPark['description']); ?></p>
+			</div>			
 		<?php endforeach; ?>
 </div> <!-- .nationalParksLoop -->
