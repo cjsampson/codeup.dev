2488e1b3561fc313d315366510731b0c0b3a9b72
CJ Sampson <cjsampson41@gmail.com>
2016-03-26 17:37:41 -0500

    Refactored try/catch blocks

--- Input.php
+++ Input.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Input
 {
     /**
@@ -25,7 +26,7 @@ class Input
 
     // Check if the input value is empty and is there
     public static function hasValue() {
-        if(isset($_REQUEST[$key]) && ($_REQUEST[$key]) != '')) {
+        if(isset($_REQUEST[$key]) && ($_REQUEST[$key]) != '') {
             return true;
         }
     }
@@ -36,58 +37,76 @@ class Input
     }
 
 
-    public static function getString($key, $min = 1, $max = 500) 
+    public static function getString($key, $min = 1, $max = 750) 
     {
         $value = self::get($key);
+
         if(!is_string($value)) {
             throw new InvalidArgumentException('The input $value needs to be a string.');
         }
-        if(!is_string($value)) {
-            throw new InvalidArgumentException('The input value $value is incorrect.');
+        if(empty($value)){
+            throw new OutOfRangeException('You need to enter a value.');
         }
-        if(!is_numeric($min) && $is_numeric($max)) {
+        if(!is_string($value) || $value == null || is_resource($value) || is_numeric($value) || 
+            is_object($value) || is_numeric($value) ) 
+        {
+            throw new DomainException('The input value $value is incorrect.');
+        }
+        if((!is_numeric($min)) && (!$is_numeric($max))) {
             throw new InvalidArgumentException('$min and $max must be numeric types.');
         }
-        if($value > $max){ 
+        if(strlen($value) > $max){ 
             throw new LengthException('The input field must be less than $max.');
         }
-        if($value < $min) {
+        if(strlen($value) < $min) {
             throw new LengthException('The input field must be more than $min.');
         }
-        
-
-
-
-        if()
-
-        $value = self::get($key);
-
-        if($value == null || is_resource($value) || is_numeric($value) || is_object($value) || is_numeric($value) ) {
-            throw new exception('The value needs to be a string');
-        } else {
-            return $value;
-        }
-
+        return $value;
+    
     }
 
-    public static function getNumber($key, $min = 1, $max = ) {
-        $value = self::get($key);       
-        if(!is_numeric($value) || $value == null) {
-            throw new exception('The value is not a number or is null');
+    public static function getNumber($key, $min = 1, $max = 4294966) {
+        $value = self::get($key);
+
+        if(!is_numeric($value)) {
+            throw new InvalidArgumentException('The input value $value is not a number.');
         }
+        if(!is_numeric($max) || !is_numeric($min)) {
+            throw new InvalidArgumentException('The max and min values are not numeric.');
+        }
+        if(empty($value)) {
+            throw new OutOfRangeException('The input field is empty.');
+        }
+        if(!is_numeric($value)) {
+            throw new DomainException('The input $value is not a numeric value.');
+        }
+        if($value > $max) {
+            throw new RangeException('The input $value is too large.');
+        }
+        if($value < $min) {
+            throw new RangeException('The input $value is too small.');
+        }
+
         return (float)$value;
     }
 
-    public static function getDate($key) {
+    public static function getDate($key, $min = '1000-01-01', $max) 
+    {
+        $max = date_create('now');
         $date = self::get($key);
         $validDate = date_create($date);
-        if(!$validDate) {
-            throw new exception('Please enter valid date');
+        if($validDate > $max) {
+            throw new DateRangeException('Date can not be in the future.');
         }
-        return $validDate;
+        if($validDate < $min) {
+            throw new DateRangeException('Date is too old.');
+        }
+        if(!is_string($validDate)) {
+            throw new DomainException('Please enter valid date');
+        }
 
+        return $validDate;
     }
-    
 
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
@@ -96,4 +115,25 @@ class Input
     // later in the curriculum.                                              //
     ///////////////////////////////////////////////////////////////////////////
     private function __construct() {}
+}
+
+class DateRangeException extends InvalidArgumentException 
+{
+    
+    public static function getDate($key, $min = '500-01-01', $max) {
+        $max = date_create('now');
+        $date = Input::get($key);       
+        if(empty($date)) {
+            throw new OutOfRangeException('Please enter a date (YYYY-MM-DD).');
+        }
+        $validDate = date_create($date);
+        if($validDate > $max) {
+            throw new DateRangeException('The date cannot be in the future.');
+        }
+        if($validDate > $min) {
+            throw new DateRangeException('The date is too far in the past.');
+        }
+
+        return $validDate;
+    }
 }
--- public/national_parks.php
+++ public/national_parks.php
@@ -2,68 +2,115 @@
 require '../config.php';
 require '../db_connect.php';
 require '../Input.php';
+// require '../DateRangeException.php';
 // require 'uploadFile.php';
 
 var_dump($_FILES);
 var_dump($_POST);
 // echo $target_file;
+$errors = [];
 
-$error = [];
-
-if(!empty($_POST)) {
-
-	if(Input::has('name') && Input::get('name') != ''
-			&& Input::has('location') && Input::get('location') != ''
-			&& Input::has('date_established') && Input::get('date_established') != ''
-			&& Input::has('area_in_acres') && Input::get('area_in_acres') != ''
-			&& Input::has('description') && Input::get('description') != ''
-			&& Input::has('images') && Input::get('images') != '' )  
-	{	
+if ( Input::get('name', '') != ''
+ 		&& Input::get('location', '') != ''
+		&& Input::get('date_established', '') != ''
+		&& Input::get('area_in_acres', '') != ''
+		&& Input::get('description', '') != ''
+		&& Input::get('images') != '' 
+	) 
+	{
+		$stmt = $dbc->prepare("INSERT INTO national_parks (name, location, date_established, area_in_acres, description, images)
+					  		   VALUES (:name, :location, :date_established, :area_in_acres, :description, :images)");
+	
 		try {
-			$parkName = Input::getString('name'); 
-		} catch(Exception $e) {
-			$error['Incorrect format ' . $e->getMessage()]; 
-		}
-		try {
-			$parkLocation = Input::getString('location'); 
-		} catch(Exception $e) {
-			$error['Incorrect Location format ' . $e->getMessage()];
-		}
-		try {
-			$parkDate = Input::getDate('date_established'); 
-		} catch(Exception $e) {
-			$error['Incorrect Date format ' . $e->getMessage()];
+			$parkName = Input::getString('name', 0, 100); 
+		} catch(InvalidArgumentException $e) {
+			$errors[] = $e->getMessage(); 
+		} catch(OutOfRangeException $e) {
+			$errors[] = $e->getMessage();  
+		} catch(DomainException $e) {
+			$errors[] = $e->getMessage(); 
+		} catch(InvalidArgumentException $e) {
+		    $errors[] = $e->getMessage(); 
+		} catch(LengthException $e) {
+			$errors[] = $e->getMessage(); 
 		} 
+		
+		try { 
+			$location = Input::getString('name', 0, 100); 
+		} catch(InvalidArgumentException $e) { 
+			$errors[] = $e->getMessage(); 
+		}  catch(OutOfRangeException $e) {
+		    $errors[] = $e->getMessage(); 
+		} catch(DomainException $e) { 
+			$errors[] = $e->getMessage(); 
+		} catch(InvalidArgumentException $e) { 
+			$errors[] = $e->getMessage(); 
+		} catch(LengthException $e) { 
+			$errors[] = $e->getMessage(); 
+		}
+
+		try {
+			$parkDate = DateRangeException::getDate('date_established'); 
+		} catch(OutOfRangeException $e) {
+			$erros = $e->getMessage();
+		} catch(DateRangeException $e) {
+			$errors = $e->getMessage();
+		} catch(DateRangeException $e) {
+			$errors = $e->getMessage();
+		}
+
 		try {
 			$parkAreaAcres = Input::getNumber('area_in_acres'); 
-		} catch(Exception $e) {
-			$error['Incorrect number format ' . $e->getMessage()];
+		} catch(InvalidArgumentException $e) {
+			$errors = $e->getMessage();
+		} catch(OutOfRangeException $e) {
+			$errors = $e->getMessage();
+		} catch(DomainException $e) {
+			$errors = $e->getMessage();
+		} catch(RangeException $e) {
+			$errors = $e->getMessage();
 		}
+
 		try {
 			$parkDescription = Input::getString('description'); 
-		} catch(Exception $e) {
-			$error['Incorrect Park Description format ' . $e->getMessage()];
+		} catch(InvalidArgumentException $e) {
+			$errors = $e->getMessage(); 
+		} catch(OutOfRangeException $e) {
+			$errors = $e->getMessage();  
+		} catch(DomainException $e) {
+			$errors = $e->getMessage(); 
+		} catch(InvalidArgumentException $e) {
+			$errors = $e->getMessage(); 
+		} catch(LengthException $e) {
+			$errors = $e->getMessage(); 
 		}
 		try {
 			$parkImage = Input::getString('images'); 
+		} catch(InvalidArgumentException $e) {
+			$errors = $e->getMessage(); 
+		} catch(OutOfRangeException $e) {
+			$errors = $e->getMessage();  
+		} catch(DomainException $e) {
+			$errors = $e->getMessage(); 
+		} catch(InvalidArgumentException $e) {
+			$errors = $e->getMessage(); 
+		} catch(LengthException $e) {
+			$errors = $e->getMessage(); 
 		}
-		catch(Exception $e) {	
-			$error['Incorrect Image format ' . $e->getMessage()];
+		if(empty($errors)) 
+		{
+			$stmt->bindValue(':name', $parkName, PDO::PARAM_STR);
+			$stmt->bindValue(':location', $parkLocation, PDO::PARAM_STR);
+			$stmt->bindValue(':date_established', $parkDate->date, PDO::PARAM_STR);
+			$stmt->bindValue(':area_in_acres', $parkAreaAcres, PDO::PARAM_STR);
+			$stmt->bindValue(':description', $parkDescription, PDO::PARAM_STR);
+			$stmt->bindValue(':images', $parkImage, PDO::PARAM_STR);
+			$stmt->execute();
 		}
-		print_r($error);
-
-	$stmt = $dbc->prepare("INSERT INTO national_parks (name, location, date_established, area_in_acres, description, images)
-				  		   VALUES (:name, :location, :date_established, :area_in_acres, :description, :images)");
-	$stmt->bindValue(':name', $parkName, PDO::PARAM_STR);
-	$stmt->bindValue(':location', $parkLocation, PDO::PARAM_STR);
-	$stmt->bindValue(':date_established', $parkDate->date, PDO::PARAM_STR);
-	$stmt->bindValue(':area_in_acres', $parkAreaAcres, PDO::PARAM_STR);
-	$stmt->bindValue(':description', $parkDescription, PDO::PARAM_STR);
-	$stmt->bindValue(':images', $parkImage, PDO::PARAM_STR);
-	$stmt->execute();
-	} // end If statment
-}
-
+	}  elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	   		$errors[] = 'All values are required...';
+	}
+	
 
 $page = (Input::has('page')) ? Input::get('page') : 1;
 $limit = (Input::has('limit')) ? Input::get('limit') : 4;
@@ -82,7 +129,23 @@ $nationalParks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt = $dbc->query("SELECT COUNT(*) FROM national_parks");
 $totalParks = $stmt->fetchColumn();
 
+$page = (Input::has('page')) ? Input::get('page') : 1;
+$limit = 4;
+$offset = ($page * $limit) - $limit;
 
+
+// Query to LIMIT results for view
+$stmt = $dbc->prepare("SELECT * FROM national_parks LIMIT :limit OFFSET :offset");
+$stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+$stmt->execute();
+
+// Associate Array of Parks
+$nationalParks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Total Parks
+$stmt = $dbc->query("SELECT COUNT(*) FROM national_parks");
+$totalParks = $stmt->fetchColumn();
 
 ?>
 <!DOCTYPE html>
@@ -90,9 +153,9 @@ $totalParks = $stmt->fetchColumn();
 <head>
 	<meta charset="UTF-8">
 	<title>Japanese National Parks</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/i18n/jquery-ui-i18n.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="/css/national_parks.css">
 </head>
@@ -109,7 +172,7 @@ $totalParks = $stmt->fetchColumn();
 				<?php foreach ($nationalParks as $nationalPark): ?>
 					<div class="nationalPark col-md-6">
 						<h4><?= Input::escape($nationalPark['name']); ?></h4>
-						<img src="<?= Input::escape($nationalPark['images']);?> " alt="<?= $nationalPark['name'];?>" class="img-responsive col-md-6">
+						<img src="<?= Input::escape($nationalPark['images']); ?>" alt="<?= $nationalPark['name'];?>" class="img-responsive col-md-6">
 						<ul class="parkUL col-md-6">
 							<li>Name: <?= Input::escape($nationalPark['name']); ?></li>
 							<li>Location: <?= Input::escape($nationalPark['location']); ?></li>
@@ -121,40 +184,58 @@ $totalParks = $stmt->fetchColumn();
 				<?php endforeach; ?>
 			</div>
 		</div> <!-- .nationalParksLoop -->
+		
+		<?php foreach($errors as $error): ?>
+			<p><?= $error; ?></p>
+		<?php endforeach; ?>
 
 		<!-- Forms for Parks -->
 		<div class="col-md-4">
 			<form action="national_parks.php?page=1" id="parksForm" method="post" name="parksForm" enctype="multipart/form-data">
+				
 				<fieldset class="form-group">
 					<h1>Add a Park to the List!</h1>
 				</fieldset>
+
 				<fieldset class="form-row">
 					<label for="name">Name: </label>
-					<input type="text" name="name" id="name" placeholder="Name of park" class="required">				
+					<input type="text" name="name" id="name" placeholder="Name of park" class="required"
+					value="<?= empty($errors) ? '' : Input::get('name', '') ?>">				
 				</fieldset>
+
 				<fieldset class="form-row">
 					<label for="location">Location: </label>
-					<input type="text" name="location" placeholder="Location" class="required">
+					<input type="text" name="location" placeholder="Location" class="required"
+					value="<?= empty($errors) ? '' : Input::get('location', '') ?>">
 				</fieldset>
+
 				<fieldset class="form-row">
 					<label for="date_established">Date Established: </label>
-					<input type="date" name="date_established" id="date_established"  class="required" placeholder="YYYY-MM-DD">
+					<input type="date" name="date_established" id="date_established"  class="required" placeholder="YYYY-MM-DD"
+					value="<?= empty($errors) ? '' : Input::get('date', '') ?>">
 				</fieldset>
+
 				<fieldset class="form-row">
 					<label for="description">Description: </label>
-					<textarea name="description" id="description" class="required"></textarea>				
+					<textarea name="description" id="description" class="required"
+					value="<?= empty($errors) ? '' : Input::get('description', '') ?>"></textarea>				
 				</fieldset>
+
 				<fieldset class="form-row">
 					<label for="area_in_acres">Area in Acres: </label>
-					<input type="text" name="area_in_acres" id="area_in_acres" class="required">				
+					<input type="text" name="area_in_acres" id="area_in_acres" class="required"
+					value="<?= empty($errors) ? '' : Input::get('area_in_acres', '') ?>">				
 				</fieldset>
+
 				<fieldset class="form-row">
-					<input type="file" name="images" id="images" value="imageOfPark" class="required">						
+					<input type="file" name="images" id="images" value="imageOfPark" class="required"
+					value="<?= empty($errors) ? '' : Input::get('images', '') ?>">						
 				</fieldset>
 
 				<fieldset class="form-row">
 					<input type="submit" value="Update" name="parksForm">
-				</fieldset>				
+				</fieldset>		
+
 			</form>
 		</div>
 
@@ -172,7 +253,7 @@ $totalParks = $stmt->fetchColumn();
 			</div>
 		</div>
 
-	</div> <!-- .container -->
+	</div> <!-- <div class="container-fluid"></div> -->
 	<script>
 		$(document).ready(function() {
 			$("#parksForm").validate();
@@ -180,4 +261,3 @@ $totalParks = $stmt->fetchColumn();
 	</script>
 </body>
 </html>
-
